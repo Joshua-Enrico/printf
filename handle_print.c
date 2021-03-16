e0d3cba63a73e487f43c01de98ef09a504dab32f
renatoleonholberton <2829@holbertonschool.com>
2021-03-16 15:13:29 +0000

    Comment block of specifiers to test checker

--- handle_print.c
+++ handle_print.c
@@ -15,15 +15,15 @@ int handle_print(const char *fmt, int ind, va_list list, char buffer[], int flag
         {'%', print_percent},
         {'i', print_int},
         {'d', print_int},
-        {'b', print_binary},
-        {'u', print_unsigned},
-        {'o', print_octal},
-        {'x', print_hexadecimal},
-        {'X', print_hexa_upper},
-        {'p', print_pointer},
-        {'S', print_non_printable},
-        {'r', print_reverse},
-        {'R', print_rot13string},
+        /*{'b', print_binary},*/
+        /*{'u', print_unsigned},*/
+        /*{'o', print_octal},*/
+        /*{'x', print_hexadecimal},*/
+        /*{'X', print_hexa_upper},*/
+        /*{'p', print_pointer},*/
+        /*{'S', print_non_printable},*/
+        /*{'r', print_reverse},*/
+        /*{'R', print_rot13string},*/
         {'\0', NULL}
     };
 
