a80b144a47108d10374c5ac0e7c1633b8dae5f21
renatoleonholberton <2829@holbertonschool.com>
2021-03-18 01:54:27 +0000

    Use buffer locally in _printf file

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
 #include "holberton.h"
 
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
  * _printf - Printf function
  * @format: format
@@ -8,7 +10,7 @@
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
-	int flags, width;
+	int flags, width, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
@@ -21,11 +23,15 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			write(1, &format[i], 1);
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
 		{
+			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i);
 			/* precision = handle_precision(); */
@@ -39,7 +45,22 @@ int _printf(const char *format, ...)
 		}
 	}
 
+	print_buffer(buffer, &buff_ind);
+
 	va_end(list);
 
 	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
